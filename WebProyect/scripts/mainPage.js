58fc5435b14a28b60beb761a92554e2e257f6074
DiegoAvmor <diegoguapo2011@live.com.mx>
2019-11-10 14:20:18 -0600

    Modificacion de la funcion de cambio de images y comportamiento de la barra de navegacion.

--- WebProyect/scripts/mainPage.js
+++ WebProyect/scripts/mainPage.js
@@ -1,37 +1,38 @@
 var i = 0;
-var images = [];
 var time = 3000;
+let imageFolderSize = 6
 
-images[0] = '../images/mainPage/gif0.gif';
-images[1] = '../images/mainPage/gif1.gif';
-images[2] = '../images/mainPage/gif2.gif';
-images[3] = '../images/mainPage/gif3.gif';
-images[4] = '../images/mainPage/gif4.gif';
-images[5] = '../images/mainPage/gif5.gif';
 
-window.onload = function(){
-    document.getElementById("navTitle").addEventListener('click', function () {
-        window.open("../pages/mainPage.html", "Main", "", true);
-    });
+let navBar = document.getElementById("navBar");
+//Funcion para cambiar la opacidad de la barra de navegacion al hacer scroll
+window.onscroll = function() {
+    if(this.scrollY > 200) {
+        navBar.style.backgroundColor = "#3c3c42";
+    } else {
+         navBar.style.backgroundColor = "transparent";
+    }
+};
+   
+//Se agrega los eventos 'click'
+document.getElementById("navTitle").addEventListener('click', function () {
+    window.open("../pages/mainPage.html", "Main", "", true);
+});
 
-    document.getElementById('signUp').addEventListener('click',function () {
-        window.open("../pages/signUp.html", "SignUp", "", true);
-    });
+document.getElementById('signUp').addEventListener('click',function () {
+    window.open("../pages/signUp.html", "SignUp", "", true);
+});
     
-    document.getElementById('login').addEventListener('click',function () {
-        window.open("../pages/login.html", "Login", "", true);
-    });
-    
-    document.getElementById('catalogueBox').addEventListener('click',function () {
-        window.open("../pages/catalogue.html", "Catalogue", "", true);
-    });
+document.getElementById('login').addEventListener('click',function () {
+    window.open("../pages/login.html", "Login", "", true);
+});
 
-    changeImg();
-}
+//Se inicia la funcion que cambia las imagenes
+changeImg();
 
 function changeImg(){
-    document.slide.src = images[i];
-    if (i < images.length -1 ) {
+    source = "../images/mainPage/gif"+i+".gif";
+    document.slide.src = source;
+    if (i < imageFolderSize -1 ) {
         i++;
     }else{
         i=0;
